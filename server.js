4cc38504c4d75182e535391da3cb62f845533174
Roger Nordqvist <progdev.squirrel@gmail.com>
2017-07-25 11:19:39 +0200

    registering graphql with express

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import expressGraphQL from 'express-graphql';
 
 const app = express();
+
+app.use('/graphql', expressGraphQL({
+  graphiql: true
+}));
 
 app.listen(4000, () => {
   console.log('+++ Server listening');
